89a96877229197919a8c94baa44a80a762ddb265
gyqls153 <ahnb14@naver.com>
2020-09-17 22:25:22 +0900

    a

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import ws from './WebSocket';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.default.baseURL = 'http://localhost:3000';
 
 function test()
 {
   //ws.send(JSON.stringify(data));
   
-  axios.get('/user')
+  axios.post('/admin_join')
   .then(function (response) { console.log(response); })
   .catch(function (error) { });
 }
@@ -28,24 +29,40 @@ function test2()
 function App() {
   return (
     <div className="App">
+       {/* <Router>
+        <div className='Contents-wrapper'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/community' component={Community} />
+            <Route path='/mypage' component={Mypage} />
+          </Switch>
+        </div>
+        <div className='Menu-wrapper'>
+          <ul>
+            <Link to='/'><li>Home</li></Link>
+            <Link to='/community'><li>Community</li></Link>
+            <Link to='/mypage'><li>MyPage</li></Link>
+          </ul>
+        </div>
+      </Router> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <input type="text" id="id" />
-        <input type="password" id="password" />
-        <input type="password" id="password_verify" />
-        <button onClick= {test}>Click me</button>
-        <button onClick= {test2}>Click me</button>
+        <div className="App-Admin-Join">
+          <div>
+            <label>ID</label>
+            <input type="text" id="id" />
+          </div>
+          <div>
+            <label>PASSWORD</label>
+            <input type="password" id="password" />
+          </div>
+          <div>
+            <label>PASSWORD_VERIFY</label>
+            <input type="password" id="password_verify" />
+          </div>
+          <button onClick= {test}>Click me</button>
+          <button onClick= {test2}>Click me</button>
+        </div>
       </header>
     </div>
   );
