28d5263e02c8d0b676070cc1d7ceb1f1822c5527
gyqls153 <ahnb14@naver.com>
2020-11-07 20:54:00 +0900

    Update Admin_Game_Box.js

--- src/Admin_Game_Box.js
+++ src/Admin_Game_Box.js
@@ -27,7 +27,7 @@ const TextMain = styled.p`
     font-size: 17px;
     position: absolute;
     left: 75px;
-    top: 10px;
+    top: 5px;
     
     @import url(//fonts.googleapis.com/earlyaccess/nanumgothic.css);
     font-family: 'Nanum Gothic', sans-serif;
